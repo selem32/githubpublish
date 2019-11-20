e106b0ecf899efe6fdf20a961e3fe46315a88e8f
selemafonso <selem32@gmail.com>
2019-11-20 09:34:40 -0300

    Publish

--- scripts/build_publish.sh
+++ scripts/build_publish.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-  echo "We're not on the master branch."
-  # analyze current branch and react accordingly
-  else
-  echo "We're on the master branch."
-  ./gradlew clean build publish -PdisablePreDex --stacktrace
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  ./gradlew publish -PdisablePreDex --stacktrace
 fi
 
