9ddd38586e28a2061af55927974ac1ae11f5f898
selemafonso <selem32@gmail.com>
2019-11-20 10:23:58 -0300

    Publish

--- scripts/build_publish.sh
+++ scripts/build_publish.sh
@@ -2,11 +2,11 @@
 
 echo $TRAVIS_BRANCH
 
-if [[ "$TRAVIS_BRANCH" = "master" ]]; then
+if [ $TRAVIS_BRANCH != "master" ]; then
     echo "We're on the master branch."
-    ./gradlew clean build publish -PdisablePreDex --stacktrace
-else
     ./gradlew clean build -PdisablePreDex --stacktrace
+else
+    ./gradlew clean build publish -PdisablePreDex --stacktrace
     echo "We're on the master branch."
 fi
 
